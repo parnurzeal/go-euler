c166284775774e1d6305e7298bcb148795baab65
ParNurZeal <parnurzeal@gmail.com>
2014-09-09 23:31:35 +0900

    Made queue and matrix for euler81

--- euler81/main.go
+++ euler81/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,17 +13,51 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(inBytes))
 	lines := strings.Split(string(inBytes), "\n")
 	// put in matrix
-	for idx, oneLine := range lines {
-		fmt.Println(idx, oneLine)
-		/*nums := strings.Split(oneLine, " ")
-		if len(nums) == 0 {
-			break
+	matrix := make([][]int, len(lines))
+	for r, oneLine := range lines {
+		nums := strings.Split(oneLine, " ")
+		matrix[r] = make([]int, len(nums))
+		for c, oneNum := range nums {
+			matrix[r][c], err = strconv.Atoi(oneNum)
+			if err != nil {
+				panic(err)
+			}
 		}
-		fmt.Println(len(nums), nums)*/
 	}
+	fmt.Println(matrix)
+}
 
-	fmt.Println(lines)
+type Graph struct {
+	nodes Node
+}
+type Node struct {
+	payload int
+	adjNode map[int]struct{}
+}
+
+type Queue []int
+
+func (q *Queue) enqueue(elem int) {
+	*q = append(*q, elem)
+}
+
+func (q *Queue) dequeue() int {
+	if q.isEmpty() {
+		panic("try to dequeue empty queue")
+	}
+	elem := (*q)[0]
+	copy((*q)[0:], (*q)[1:])
+	(*q)[len(*q)-1] = 0
+	(*q) = (*q)[:len(*q)-1]
+	return elem
+}
+
+func (q *Queue) isEmpty() bool {
+	if len(*q) == 0 {
+		return true
+	} else {
+		return false
+	}
 }
